f2dc0c2734d46422846adfbb2bc2605e90800ff6
hemis23 <themischa9@gmail.com>
2024-02-26 20:39:44 +0200

    sdasd

--- server.js
+++ server.js
@@ -29,7 +29,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://smart-brain-6kzw.onrender.com/'
+};
+
+app.use(cors(corsOptions));
 
 
 app.get("/", (req,res)=>{res.json("success")})
