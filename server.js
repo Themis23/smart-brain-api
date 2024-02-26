e78b7ef834d3e7c473bf7aeebc8fd50666dfc3f2
hemis23 <themischa9@gmail.com>
2024-02-26 20:18:04 +0200

    dasdasds

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({origin:"https://smart-brain-6kzw.onrender.com/"}));
+app.use(cors());
 
 
 app.get("/", (req,res)=>{res.json("success")})
