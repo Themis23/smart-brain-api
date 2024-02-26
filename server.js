7f955b6371d40a0557f8c3f04c182063f5e7480d
hemis23 <themischa9@gmail.com>
2024-02-26 19:08:46 +0200

    connecting to server with env variable

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : 'postgres://postgresql_d9e7_user:nm8WdLsSM9lwrTKLAdVmSfmIILG9FZNy@dpg-cne4c5icn0vc73f9a9m0-a/postgresql_d9e7'
+    connectionString : process.env.DATABASE_URL,
   }
 });
 
