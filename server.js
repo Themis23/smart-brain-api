68c3c916ecbe24fc513d714facf10643c3989b8e
hemis23 <themischa9@gmail.com>
2024-02-26 18:45:30 +0200

    adding server url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgres://postgresql_d9e7_user:nm8WdLsSM9lwrTKLAdVmSfmIILG9FZNy@dpg-cne4c5icn0vc73f9a9m0-a/postgresql_d9e7',
     port : 5432,
     user : 'postgres',
     password : 'test',
